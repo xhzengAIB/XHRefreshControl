49b7bb9df3f4f4695268971d93b4f54c26c3ef57
Jack <xhzengAIB@gmail.com>
2015-01-06 16:55:23 +0800

    .

--- XHRefreshControlExample/XHRefreshControlExample/Controllers/SimulationNetEaseNetworkErrorViewController/XHSimulationNetEaseNetworkErrorViewController.m
+++ XHRefreshControlExample/XHRefreshControlExample/Controllers/SimulationNetEaseNetworkErrorViewController/XHSimulationNetEaseNetworkErrorViewController.m
@@ -2,7 +2,7 @@
 //  SimulationNetEaseNetworkErrorViewController.m
 //  XHRefreshControlExample
 //
-//  Created by kingsoft_ios on 15/1/6.
+//  Created by 曾 宪华 on 15/1/6.
 //  Copyright (c) 2015年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
 //
 
